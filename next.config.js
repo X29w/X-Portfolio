9f41fbee14dc9ea7b04a7eaf80f9246a9c1c6fad
園洛樱介 <1937009360@qq.com>
2025-01-04 18:10:01 +0800

    [add]github action

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const repo = "X-Portfolio";
+let assetPrefix = `/${repo}/`;
+
+let basePath = `/${repo}`;
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  assetPrefix,
+  basePath,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
